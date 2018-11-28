c774c021c7d2de024226b859d860820214461a7b
Brendon <bbrendonshi@gmail.com>
2018-11-28 21:38:49 +1100

    added basic shape constants

--- tetris.js
+++ tetris.js
@@ -1,5 +1,62 @@
 const canv = document.getElementById("tetrjs");
 const context = canv.getContext("2d");
 
+context.scale(20, 20);
 context.fillStyle = '#000';
 context.fillRect(0, 0, canv.width, canv.height);
+
+
+const T = [
+	[0,0,0],
+	[1,1,1],
+	[0,1,0],
+];
+
+const O = [
+	[1,1],
+	[1,1],
+];
+
+const L = [
+	[0,1,0],
+	[0,1,0],
+	[0,1,1],
+];
+
+const J = [
+	[0,1,0],
+	[0,1,0],
+	[1,1,0],
+];
+
+const Z = [
+	[0,0,0],
+	[1,1,0],
+	[0,1,1],
+];
+
+const S = [
+	[0,0,0],
+	[0,1,1],
+	[1,1,0],
+];
+
+const I = [
+	[0,1,0],
+	[0,1,0],
+	[0,1,0],
+];
+
+function drawShape(matrix, offset, colour){
+	matrix.forEach((row, y) => {
+		row.forEach((value, x) =>{
+			if(value !== 0){
+				context.fillStyle = colour;
+				context.fillRect(x + offset.x, y + offset.y, 1, 1);
+			}
+		});
+	});
+}
+
+
+drawShape(L, {x: 5, y: 5}, "blue");
